f9e7b6a17d32eeb752435e510c800932bf4ed21d
Vighneshgits <141628294+Vighneshgit@users.noreply.github.com>
2023-08-07 15:50:05 +0530

    Add files via upload

---
+++ smoke/smoke.ino
@@ -0,0 +1,30 @@
+int buzzer = LED_BUILTIN;
+int smoke = A0;
+
+// Your threshold value. You might need to change it.
+int sensorThres = 750;
+
+void setup() {
+ pinMode(buzzer, OUTPUT);
+ pinMode(smoke, INPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+ int analogSensor = analogRead(smoke);
+
+ Serial.print("Pin A0: ");
+ Serial.println(analogSensor);
+ // Checks if it has reached the threshold value
+ if (analogSensor > sensorThres)
+ {
+   //tone(buzzer, 1000, 200);
+   digitalWrite(buzzer,HIGH);
+ }
+ else
+ {
+   //noTone(buzzer);
+   digitalWrite(buzzer,LOW);
+ }
+ delay(100);
+}
